db2393bff5dc83471a4f6c421b18203b3afe13d3
Kysaka <vlad.gorlach.2013@gmail.com>
2023-05-26 11:58:50 +0300

    Updating a task38

--- Seminar8.py
+++ Seminar8.py
@@ -2,28 +2,29 @@ import os
 
 # создаем функцию для записи данных в файл
 def save_data(phonebook):
-    filename = input("Введите название файла для сохранения (без расширения): ")
+    filename = ("Справочник.txt")
     try:
-        with open(filename + ".txt", "w") as file:
+        with open(filename, 'w', encoding='utf-8') as file:
             for contact in phonebook:
                 file.write(contact["name"] + "," + contact["surname"] + "," + contact["phone"] + "\n")
-        print(f"Справочник сохранен в файле {filename}.txt")
+        print("Телефонный справочник успешно сохранен.")
     except:
         print("Невозможно сохранить файл")
 
+    
+    #     file.writelines(phonebook)
+    # print("Телефонный справочник успешно сохранен.")
+
 # создаем функцию для чтения данных из файла
 def load_data():
-    filename = input("Введите название файла для открытия (без расширения): ")
-    try:
-        with open(filename + ".txt", "r") as file:
-            phonebook = []
-            for line in file:
-                data = line.strip().split(",")
-                phonebook.append({"name":data[0], "surname":data[1], "phone":data[2]})
-            print(f"Справочник загружен из файла {filename}.txt")
-            return phonebook
-    except:
-        print("Невозможно открыть файл")
+    filename = ("Справочник.txt")
+    if os.path.exists(filename):
+        with open(filename, 'r') as file:
+            print("Телефонный справочник успешно открыт.")
+        return phonebook
+    else:
+        print("Файл не существует. Создайте новый справочник.")
+        return []
 
 # создаем функцию для поиска контакта
 def find_contact(phonebook):
@@ -74,23 +75,114 @@ phonebook = []
 if os.path.isfile("phonebook.txt"):
     phonebook = load_data()
 
-while True:
-    print("""Меню:
-1. Поиск контакта
-2. Добавление контакта
-3. Удаление контакта
-4. Сохранение справочника
-5. Выход""")
-    choice = input("Выберите пункт меню: ")
-    if choice == "1":
-        find_contact(phonebook)
-    elif choice == "2":
-        add_contact(phonebook)
-    elif choice == "3":
-        delete_contact(phonebook)
-    elif choice == "4":
-        save_data(phonebook)
-    elif choice == "5":
-        exit_program()
-    else:
-        print("Неверный ввод") 
+def main():
+    phonebook = []
+    while True:
+        print("\nТелефонный справочник")
+        print("1. Открыть справочник")
+        print("2. Сохранить справочник")
+        print("3. Добавить запись")
+        print("4. Удалить запись")
+        print("5. Поиск записей")
+        print("6. Выйти из программы")
+        
+        choice = input("Введите номер операции: ")
+        
+        if choice == "1":
+            phonebook = load_data()
+        elif choice == "2":
+            save_data(phonebook)
+        elif choice == "3":
+            add_contact(phonebook)
+        elif choice == "4":
+            delete_contact(phonebook)
+        elif choice == "5":
+            find_contact(phonebook)
+        elif choice == "6":
+            break
+        else:
+            print("Неверный ввод. Пожалуйста, выберите номер операции от 1 до 6.")
+
+if __name__ == '__main__':
+    main()
+
+# import os
+
+# def open_phonebook():
+#     filename = input("Введите имя файла: ")
+#     if os.path.exists(filename):
+#         with open(filename, 'r') as file:
+#             phonebook = file.readlines()
+#         print("Телефонный справочник успешно открыт.")
+#         return phonebook
+#     else:
+#         print("Файл не существует. Создайте новый справочник.")
+#         return []
+
+# def save_phonebook(phonebook):
+#     filename = input("Введите имя файла для сохранения: ")
+#     with open(filename, 'w', encoding='utf-8') as file:
+#         file.writelines(phonebook)
+#     print("Телефонный справочник успешно сохранен.")
+
+# def add_entry(phonebook):
+#     name = input("Введите имя: ")
+#     surname = input("Введите фамилию: ")
+#     phone = input("Введите номер телефона: ")
+#     entry = f"{name} {surname}: {phone}\n"
+#     phonebook.append(entry)
+#     print("Запись успешно добавлена.")
+
+# def delete_entry(phonebook):
+#     search_term = input("Введите имя, фамилию или номер телефона для удаления: ")
+#     entries_to_remove = []
+#     for entry in phonebook:
+#         if search_term in entry:
+#             entries_to_remove.append(entry)
+#     if entries_to_remove:
+#         for entry in entries_to_remove:
+#             phonebook.remove(entry)
+#         print("Записи успешно удалены.")
+#     else:
+#         print("Записей не найдено.")
+
+# def search_entries(phonebook):
+#     search_term = input("Введите имя, фамилию или номер телефона для поиска: ")
+#     found_entries = []
+#     for entry in phonebook:
+#         if search_term in entry:
+#             found_entries.append(entry)
+#     if found_entries:
+#         print("Найденные записи:")
+#         for entry in found_entries:
+#             print(entry)
+#     else:
+#         print("Записей не найдено.")
+
+# def main():
+#     phonebook = []
+#     while True:
+#         print("\n Телефонный справочник")
+#         print("1. Открыть справочник")
+#         print("2. Сохранить справочник")
+#         print("3. Добавить запись")
+#         print("4. Удалить запись")
+#         print("5. Поиск записей")
+#         print("6. Выйти из программы")
+        
+#         choice = input("Введите номер операции: ")
+        
+#         if choice == "1":
+#             phonebook = open_phonebook()
+#         elif choice == "2":
+#             save_phonebook(phonebook)
+#         elif choice == "3":
+#             add_entry(phonebook)
+#         elif choice == "4":
+#             delete_entry(phonebook)
+#         elif choice == "5":
+#             search_entries(phonebook)
+#         elif choice == "6":
+#             break
+#         else:
+#             print("Неверный ввод. Пожалуйста, выберите номер операции от 1 до 6.")
